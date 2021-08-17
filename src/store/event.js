26059946bc620bddfeabd76c1e2ca1bbd0e0f23b
Makoto-Onohara <64698747+Makoto-Onohara@users.noreply.github.com>
2021-08-17 23:52:25 +0900

    created a daialog component

---
+++ src/store/event.js
@@ -0,0 +1,25 @@
+export const event = {
+  namespaced: true,
+  state: {
+    events: [
+      {
+        eventNo: 1,
+        name: "Vue&TS勉強会",
+        start: "2021-09-01",
+        end: "2021-09-21",
+        color: "blue",
+        detail: "",
+        timed: false,
+      },
+      {
+        eventNo: 2,
+        name: "Vue.js応用勉強会",
+        start: "2021-08-01",
+        end: "2021-08-07",
+        color: "green",
+        detail: "",
+        timed: false,
+      },
+    ],
+  },
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { event } from "@/store/event.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  // 変数を定義する場所、参照専用で直接変更してはいけない
+  state: {
+    isOpenSideBar: true,
+    createTaskDialog: false,
+  },
+  // stateの変更する処理のみを記述
+  mutations: {
+    updateIsOpenSideBar(state, value) {
+      state.isOpenSideBar = value;
+    },
+    updateCreateTaskDialog(state, value) {
+      state.createTaskDialog = value;
+    },
+  },
+  // コンポーネントで使用する関数を定義する
+  actions: {
+    switchSideBar({ commit }, payload) {
+      // 第２引数がmutationsの関数の第２引数に入る
+      commit("updateIsOpenSideBar", payload);
+    },
+    switchCreateTaskDialog({ commit }, payload) {
+      commit("updateCreateTaskDialog", payload);
+    },
+  },
+  modules: {
+    event,
+  },
 });
